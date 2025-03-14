40a48ad65566a6eaa276e3fdbf12d3f2c8e95011
Nicola0612 <andreea-nicola.moisoiu@student.tuiasi.ro>
2025-03-14 12:19:59 +0200

    main

---
+++ Main.java
@@ -0,0 +1,8 @@
+package PaooGame;
+
+public class Main {
+    public static void main(String[] args) {
+          Game paooGame = Game.getInstance("În căutarea ștrumfiței", 960, 768);
+        paooGame.StartGame();
+    }
+}
